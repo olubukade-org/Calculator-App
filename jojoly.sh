94bf828223a5ad615a1eebf023a440a608ebacc3
bukkia <bukkiadeniyi@yahoo.com>
2025-10-28 01:23:29 -0700

    Dev (#3) (#4)
    * New file created
    
    * New file updated
    
    * New file updated in new-feature

---
+++ jojoly.sh
@@ -0,0 +1,11 @@
+ echo "You get to know who truly loves you during hard times"
+
+ echo "Nevertheless God has been faithful"
+
+ echo "I love you Lord, the owner of my soul"
+
+ pwd
+
+ mkdir abiolao
+
+ touch bukky > abiolao
